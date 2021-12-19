0782b2286dc623bc4c092549988d1e7128aae055
unfamiliarish <89762294+unfamiliarish@users.noreply.github.com>
2021-12-19 13:07:34 -0600

    tweaks

--- aoc/__main__.py
+++ aoc/__main__.py
@@ -1,11 +1,15 @@
 import click
 
+DAYS_SOLVED = 3
+
 @click.command()
 @click.argument('day')
 def run_aoc(day: str) -> None:
     day = int(day)
-    if not (1 <= day or day <= 25):
+    if not (1 <= day and day <= 25):
         raise RuntimeError("Day must be a value between 1-25.")
+    if not (day <= DAYS_SOLVED):
+        raise RuntimeError(f"There are only solutions up to day {DAYS_SOLVED}. Choose a smaller number.")
 
     # import the right solution code to execute
     package = f"aoc.day{day}.solution"
