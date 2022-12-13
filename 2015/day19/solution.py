902b20ea0846b8e844d3ca680dd899b7cb24144b
unfamiliarish <89762294+unfamiliarish@users.noreply.github.com>
2022-12-12 22:14:36 -0800

    backwards replacement also doesn't work

--- 2015/day19/solution.py
+++ 2015/day19/solution.py
@@ -13,6 +13,15 @@ def build_molecule_map(raw_mapping: list[str]) -> dict[str, list[str]]:
     return molecule_map
 
 
+def build_reverse_molecule_map(raw_mapping: list[str]) -> dict:
+    reverse_molecule_map = {}
+    for raw_map in raw_mapping:
+        base, replacement = raw_map.split(" => ")
+        reverse_molecule_map[replacement] = base
+
+    return reverse_molecule_map
+
+
 def find_all(input: str, substr: str) -> list[int]:
     locations = []
 
@@ -95,7 +104,21 @@ def get_num_distinct_molecules(filename: str) -> int:
 # and store them accordingly
 
 
-def steps_to_make_medicine(goal_molecule: str, molecule_map: dict[str, list[str]]):
+def filter_invalid_molecules(molecules: set[str], base_molecule: str) -> set[str]:
+    valid_molecules = set()
+    for molecule in molecules:
+        if molecule.find(base_molecule) >= 0:
+            if len(molecule) != len(base_molecule):
+                continue
+
+        valid_molecules.add(molecule)
+
+    return valid_molecules
+
+
+def steps_to_make_medicine(
+    goal_molecule: str, base_molecule: str, molecule_map: dict[str, list[str]]
+) -> int:
     """example dictionaries below for illustration,
     not accuracy between each other
 
@@ -113,62 +136,64 @@ def steps_to_make_medicine(goal_molecule: str, molecule_map: dict[str, list[str]
     }
     """
 
-    molecules_at_depth = defaultdict(list)
+    molecules_at_step = defaultdict(list)
 
-    molecule_shortest_depth = {"e": 0}
-    molecules_at_depth[0].append("e")
+    molecule_fewest_steps = {goal_molecule: 0}
+    molecules_at_step[0].append(goal_molecule)
 
     curr_depth = 0
-    while goal_molecule not in molecule_shortest_depth:
+    while base_molecule not in molecule_fewest_steps:
         print(curr_depth)
-        # pprint(molecules_at_depth[curr_depth])
-        molecules_at_curr_depth = molecules_at_depth[curr_depth]
+        pprint(len(molecules_at_step[curr_depth]))
+        molecules_at_curr_step = molecules_at_step[curr_depth]
         # breakpoint()
-        for molecule in molecules_at_curr_depth:
+        for molecule in molecules_at_curr_step:
             next_depth = curr_depth + 1
             next_depth_molecules = calc_set_of_distinct_molecules(
                 molecule, molecule_map
             )
-            for next_molecule in next_depth_molecules:
-                # breakpoint()
-                if next_molecule == goal_molecule:
-                    return curr_depth + 1
-                if next_molecule not in molecule_shortest_depth:
-                    molecule_shortest_depth[next_molecule] = next_depth
-                    molecules_at_depth[next_depth].append(next_molecule)
+            filtered_molecules = filter_invalid_molecules(
+                next_depth_molecules, base_molecule
+            )
 
-        if curr_depth == 1000:
-            # breakpoint()
-            5
+            for next_molecule in filtered_molecules:
+                # breakpoint()
+                if next_molecule == base_molecule:
+                    return curr_depth + 1
+                if next_molecule not in molecule_fewest_steps:
+                    molecule_fewest_steps[next_molecule] = next_depth
+                    molecules_at_step[next_depth].append(next_molecule)
 
         curr_depth += 1
 
-    return molecule_shortest_depth[goal_molecule]
+    # didn't work
+    return -1
 
 
 def steps_to_make_medicine_in_file(filename: str) -> int:
     raw_data = utils.import_file(filename)
     molecule = raw_data[-1]
-    molecule_map = build_molecule_map(raw_data[:-2])
+    # molecule_map = build_molecule_map(raw_data[:-2])
+    reverse_molecule_map = build_reverse_molecule_map(raw_data[:-2])
 
-    return steps_to_make_medicine(molecule, molecule_map)
+    return steps_to_make_medicine(molecule, "e", reverse_molecule_map)
 
 
-sm_input = utils.import_file("input1_sm")
-assert build_molecule_map(sm_input[:-2]) == {
-    "H": ["HO", "OH"],
-    "O": ["HH"],
-    "F": ["HH"],
-}
+# sm_input = utils.import_file("input1_sm")
+# assert build_molecule_map(sm_input[:-2]) == {
+#     "H": ["HO", "OH"],
+#     "O": ["HH"],
+#     "F": ["HH"],
+# }
 
-assert find_all("abdbfbdhh", "bd") == [1, 5]
-assert find_all("abdbfbdhh", "w") == []
+# assert find_all("abdbfbdhh", "bd") == [1, 5]
+# assert find_all("abdbfbdhh", "w") == []
 
-assert get_num_distinct_molecules("input1_sm") == 4
-assert get_num_distinct_molecules("input1_sm2") == 7
+# assert get_num_distinct_molecules("input1_sm") == 4
+# assert get_num_distinct_molecules("input1_sm2") == 7
 
-part_1_result = get_num_distinct_molecules("input")
-print(f"part 1: {part_1_result}")
+# part_1_result = get_num_distinct_molecules("input")
+# print(f"part 1: {part_1_result}")
 
 
 assert steps_to_make_medicine_in_file("input2_sm") == 3
